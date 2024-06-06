513f6dd0bc59b3441926b487bbb3c397321b9d53
muxiatong <1347904922@qq.com>
2024-06-06 22:19:23 +0800

    langchian llm 加载增加智谱适配

--- langchain_load_llm.py
+++ langchain_load_llm.py
@@ -3,12 +3,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
 from langchain_community.llms import Tongyi
 from langchain_community.llms import QianfanLLMEndpoint
-from langchain_core.language_models.llms import BaseLLM
-from langchain_core.outputs import Generation, LLMResult
-from langchain_core.callbacks import CallbackManagerForLLMRun
-from typing import Any, List, Optional
 from transformers import pipeline
 from langchain import HuggingFacePipeline
+from langchain_openai import ChatOpenAI
 
 
 class LLM():
@@ -17,6 +14,7 @@ class LLM():
         self.DASHSCOPE_API_KEY = os.getenv('DASHSCOPE_API_KEY')
         self.QIANFAN_AK = os.getenv('QIANFAN_AK')
         self.QIANFAN_SK = os.getenv('QIANFAN_SK')
+        self.ZHIPUAI_API_KEY = os.getenv('ZHIPUAI_API_KEY')
 
     def tongyi_qwen_plus(self, temperature=1):
         # 通义 qwen_plus
@@ -60,6 +58,22 @@ class LLM():
         # 文心 ERNIE-Lite-8K
         model = QianfanLLMEndpoint(streaming=True, model='ERNIE-Speed-8K', qianfan_ak=self.QIANFAN_AK,
                                    qianfan_sk=self.QIANFAN_SK)
+        return model
+
+    def zhipu_glm_3_turbo(self, temperature=0.9):
+        # 智谱的 temperature 最高不能 >= 1
+        model = ChatOpenAI(temperature=temperature, model="glm-3-turbo",
+            openai_api_key=self.ZHIPUAI_API_KEY,
+            openai_api_base="https://open.bigmodel.cn/api/paas/v4/"
+        )
+        return model
+
+    def zhipu_glm_4(self, temperature=0.9):
+        # 智谱的 temperature 最高不能 >= 1
+        model = ChatOpenAI(temperature=temperature, model="glm-4",
+            openai_api_key=self.ZHIPUAI_API_KEY,
+            openai_api_base="https://open.bigmodel.cn/api/paas/v4/"
+        )
         return model
 
     def local_llm(self, model_path, temperature=0.6):
